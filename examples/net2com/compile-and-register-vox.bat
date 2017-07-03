e493ec8bff951509cc0ea183cfe88b1a3947b5b2
Karl Oswald <KarlFOswald@gmail.com>
2017-07-02 17:27:56 -0700

    support system env. variables

--- examples/net2com/compile-and-register-vox.bat
+++ examples/net2com/compile-and-register-vox.bat
@@ -2,4 +2,4 @@
 :: call compile-and-register.vbs from the command line
 :: to register a C# file with one assembly reference
 
-compile-and-register.vbs Vox.cs -ref "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0\System.Speech.dll"
+compile-and-register.vbs Vox.cs -ref "%ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0\System.Speech.dll"
