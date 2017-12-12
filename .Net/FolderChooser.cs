c9f6d3452da55b2c11902532f1b2903867105fdf
Karl Oswald <KarlFOswald@gmail.com>
2017-12-12 10:45:06 -0800

    make use of existing null check

--- .Net/FolderChooser.cs
+++ .Net/FolderChooser.cs
@@ -57,7 +57,7 @@ namespace VBScripting
         /// <returns>true if the user clicks OK</returns>
         private bool Show(IntPtr hWndOwner)
         {
-            var dir1 = Environment.ExpandEnvironmentVariables(_initialDirectory);
+            var dir1 = Environment.ExpandEnvironmentVariables(InitialDirectory);
             var result = Environment.OSVersion.Version.Major >= 6
                 ? VistaDialog.Show(hWndOwner, dir1, Title)
                 : ShowXpDialog(hWndOwner, dir1, Title);
