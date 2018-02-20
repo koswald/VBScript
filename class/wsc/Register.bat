8bc5908ae14e07fdcba6c46662b2b7f7ddbb1fa0
Karl Oswald <turnbuckle@hotmail.com>
2018-02-19 16:21:00 -0800

    comment

--- class/wsc/Register.bat
+++ class/wsc/Register.bat
@@ -1,4 +1,7 @@
-:: Call Register.vbs
-@If %1.==. echo Arg required. Press a key to exit & pause > nul & exit /b
-@For %%A in (%1) do @echo Registering %%~nxA
-@..\..\examples\Register.vbs %1
+:: register.vbs a single .wsc file passed in on the command line
+@echo off
+If %1.==. echo Arg required. Press a key to exit & pause > nul & exit /b
+For %%A in (%1) do (
+    echo Registering %%~nxA
+)
+..\..\examples\Register.vbs %1
